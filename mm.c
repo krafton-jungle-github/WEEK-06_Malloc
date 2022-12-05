87ff141e13f4c3f7c5fcac6dd433b69b693ca715
nickhealthy <alskadmlcraz1@gmail.com>
2022-12-06 02:18:16 +0900

    FEAT: 전처리기(상수&매크로)

--- mm.c
+++ mm.c
@@ -9,6 +9,8 @@
  * NOTE TO STUDENTS: Replace this header comment with your own header
  * comment that gives a high level description of your solution.
  */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -22,35 +24,33 @@
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
  ********************************************************/
-team_t team = {
-    /* Team name */
-    "ateam",
-    /* First member's full name */
-    "Harry Bovik",
-    /* First member's email address */
-    "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""
-};
 
-/* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+/* 기본 size 상수 정의 */
+#define WSIZE                4              /* Word and header/footer size (bytes) */
+#define DSIZE                8              /* Double word size (bytes) */
+#define CHUNKSIZE           (1 << 12)       /* Extend heap by this amount (bytes) */ // 512 bytes
 
-/* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define MAX(x, y)           ((x) > (y) ? (x) : (y)) // x, y 중 큰 값을 출력
 
+/* 매크로 정의 */
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc)   ((size) | (alloc))                  // size와 status_bit를 통합해서 헤더|풋터에 저장하는 값을 리턴
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+/* Read and write a word at address p */
+#define GET(p)              (*(unsigned int *)(p))              // 인자 p가 참조하는 word를 읽어서 리턴
+#define PUT(p, val)         (*(unsigned int *)(p) = (val))      // 인자 p가 가리키는 word에 val를 저장
 
-/* 
- * mm_init - initialize the malloc package.
- */
-int mm_init(void)
-{
-    return 0;
-}
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p)         (GET(p) & ~0x7)                     // 주소 p에 있는 헤더 또는 풋터의 size 리턴
+#define GET_ALLOC(p)        (GET(p) & 0x1)                      // 주소 p에 있는 할당 비트를 리턴
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)            ((char *)(bp) - WSIZE)                           // 자신의 헤더를 가리키는 포인터를 리턴 // HRDP는 bp 포인터에서 -1 워드로 움직임
+#define FTRP(bp)            ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)     // 자신의 풋터를 가리키는 포인터를 리턴 // FTRP는 bp 포인터에서 헤더의 사이즈를 가져와 다음 블록의 bp까지 움직인 다음 -DSIZE만큼가서 자신의 풋터로 갈 수 있음
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)       ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))   // 다음 블럭 bp포인터 리턴 // 현재 블록의 header에서 size을 가지고 온 뒤 현재 bp 포인터에서 size만큼을 더해 다음 블록으로 포인터가 넘어감
+#define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))   // 이전 블럭 bp포인터 리턴 // 이전 블록의 footer에서 size을 가지고 온 뒤 현재 bp 포인터에서 size만큼을 빼서 이전 블록으로 포인터가 넘어감
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
@@ -94,17 +94,3 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(oldptr);
     return newptr;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
